76d3dead567966e9fd2c8ed57f3a201e8dc7ed24
alokranjan04 <alokranjan04@gmail.com>
2018-01-08 15:16:33 +0530

    JSON manupulated for multiple data analysis

--- app.js
+++ app.js
@@ -27,20 +27,113 @@
                 "Name": "Neha",
                 "Marks": 32,
                 "Subject": "Computer"
-              
-            },
-      
-            {
-                "Name": "Neha",
-                "Marks": 29,
-                "Subject": "Chemistry"
-              
-            },
-        ] 
-
-
-  CreateTableFromJSON(myBooks);
+            }
+        ]
   
+  
+  var myDocs = [
+            {
+                "LDAP": "Alok",
+                "Score": 23,
+                "Course": "Computers",
+                "Time Taken": 12
+               
+            },
+            {
+                "LDAP": "Alok",
+                "Score": 28,
+                "Course": "Chemistry",
+                "Time Taken": 29
+               
+            },
+            {
+                "LDAP": "Neha",
+                "Score": 24,
+                "Course": "History",
+                "Time Taken": 31
+              
+            },
+            {
+                "LDAP": "Alok",
+                "Score": 26,
+                "Course": "History",
+                "Time Taken": 45
+              
+            },
+            {
+                "LDAP": "Neha",
+                "Score": 26,
+                "Course": "Chemistry",
+                "Time Taken": 13
+              
+            },
+            {
+                "LDAP": "Sunil",
+                "Score": 24,
+                "Course": "Histoy",
+                "Time Taken": 17
+              
+            },
+            {
+                "LDAP": "Sunil",
+                "Score": 24,
+                "Course": "Chemistry",
+                "Time Taken": 17
+              
+            }
+        ]
+  
+ function getTotal(val, name1, name2, numVal1,numVal2) {
+    totalArr1 =  val.reduce(function(r,o){if(r){r+= o[numVal1];} else {r= o[numVal1];} return r;},(0));
+    totalArr2 =  val.reduce(function(r,o){if(r){r+= o[numVal2];} else {r= o[numVal2];} return r;},(0));
+    var obj={};
+    obj[numVal1] = totalArr1;
+    obj[numVal2] = totalArr2;
+    obj[name1] = "All";
+    obj[name2] = "All";
+    val.push(obj);
+    
+    return val;
+  
+     
+ }
+  
+  function getMarksById(val, name, numVal ){  
+    var valObj = val.reduce(
+          function(r, o){
+              if(r[o[name]]){
+                  r[o[name]] +=o[numVal]; }
+              else {r[o[name]] = o[numVal]; }
+              return r;
+          },{}); 
+    var finalObj =  Object.keys(valObj).map(function(v,i){var obj={};obj[name]= v;obj[numVal]= valObj[v]; return obj; });
+    totalArr =  finalObj.reduce(function(r,o){if(r){r+= o[numVal];} else {r= o[numVal];} return r;},(0));
+    var obj={};
+    obj[numVal] = totalArr;
+    obj[name] = "All";
+    finalObj.push(obj);
+    
+    return finalObj;
+  }
+ 
+
+
+
+//CreateTableFromJSON(getMarksById(myBooks,"Subject","Marks"));
+
+//CreateTableFromJSON(getMarksById(myBooks,"Subject","Marks"));
+
+//CreateTableFromJSON(getMarksById(myDocs,"LDAP","Time Taken"));
+
+// CreateTableFromJSON(getMarksById(myDocs,"Course","Time Taken"));
+
+//CreateTableFromJSON(myBooks);
+//CreateTableFromJSON(getTotal(myDocs,"LDAP","Course","Time Taken","Score"));
+
+CreateTableFromJSON(myDocs);
+
+
+
 function CreateTableFromJSON(jsonElementArray) {
 
         // EXTRACT VALUE FOR HTML HEADER. 
@@ -57,7 +150,7 @@ function CreateTableFromJSON(jsonElementArray) {
         // CREATE DYNAMIC TABLE.
         var table = document.createElement("table");
         
-            table.setAttribute("id","example");
+            table.setAttribute("id","tableData");
             table.setAttribute("class","table table-striped table-bordered");
             
         // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
@@ -101,12 +194,15 @@ function CreateTableFromJSON(jsonElementArray) {
         document.querySelector("thead").appendChild(trThead);        
                 
                 
-          var newtable = $('#example').DataTable({
+          var newtable = $('#tableData').DataTable({
                 lengthChange: false,
                 buttons: [ 'excel', 'pdf', 'colvis' ]
             });
 
-            newtable.buttons().container().appendTo( '#example_wrapper .col-sm-6:eq(0)' );        
+            newtable.buttons().container().appendTo( ' .col-sm-6:eq(0)' );        
              
                 
     }
+
+  //CreateTableFromJSON(anotherBooks, "showNewData");
+
