2adc4950ce01727be7e53a1527521c99dddf21f6
alokranjan04 <alokranjan04@gmail.com>
2018-03-19 13:10:39 +0530

    index.html

--- app.js
+++ app.js
@@ -128,6 +128,24 @@ function dropDownChangd() {
     }
     
 }
+      
+      
+      
+
+      
+document.getElementById("dropDownButton").addEventListener("click",function(){
+     document.getElementById("myDropdown").classList.toggle("show");
+})      
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
+}
 
 dropDownChangd();
 
